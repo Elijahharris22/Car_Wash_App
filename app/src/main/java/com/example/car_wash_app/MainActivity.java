efbb62e397a30b92db4b414bf27ccac77c71909a
Elijahharris22 <112778604+Elijahharris22@users.noreply.github.com>
2023-10-31 08:23:40 -0400

    Initial commit

---
+++ app/src/main/java/com/example/car_wash_app/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.car_wash_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.DecimalFormat;
+
+public class MainActivity extends AppCompatActivity {
+    double ExteriorWASH;
+    double ExteriorWashVacuum;
+    int washes;
+    double Total;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final EditText NumofWashes = (EditText) findViewById(R.id.tvWash);
+        final RadioButton rbExteriorwash = (RadioButton) findViewById(R.id.rbExteriorwash);
+        final RadioButton rbExteriorvac = (RadioButton) findViewById(R.id.rbExteriorvac);
+        final TextView result = (TextView) findViewById(R.id.tvResult);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                washes = Integer.parseInt(NumofWashes.getText().toString());
+                DecimalFormat cost = new DecimalFormat("$###,###.00");
+                if (rbExteriorwash.isChecked()) {
+                    if (washes >= 12) {
+                        ExteriorWASH = 8.99;
+                        Total = washes * ExteriorWASH;
+                        result.setText(cost.format(Total) + " for " + washes + " washes");
+                    } else {
+                        Toast.makeText(MainActivity.this, "Buy 12 or more washes to receive discount prices",
+                                Toast.LENGTH_LONG).show();
+                        ExteriorWashVacuum = 10.99;
+                        Total = washes * ExteriorWASH;
+                        result.setText(cost.format(Total) + " for " + washes + " washes");
+                    }
+                }
+                if (rbExteriorvac.isChecked()) {
+                    if (washes >= 12) {
+                        ExteriorWashVacuum = 12.99;
+                        Total = washes * ExteriorWashVacuum; // Corrected variable name here
+                        result.setText(cost.format(Total) + " for " + washes + " washes");
+                    } else {
+                        Toast.makeText(MainActivity.this, "Buy 12 or more washes to receive discount prices",
+                                Toast.LENGTH_LONG).show();
+                        ExteriorWashVacuum = 15.99;
+                        Total = washes * ExteriorWashVacuum; // Corrected variable name here
+                        result.setText(cost.format(Total) + " for " + washes + " washes");
+                    }
+                }
+            }
+        });
+    }
+}
